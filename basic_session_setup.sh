692a783d82f29b9e95383fcb113b27c35e8439a5
arif <arifkhalid99@gmail.com>
2025-02-01 14:00:49 +0800

    Refactor for extensibility

--- basic_session_setup.sh
+++ basic_session_setup.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-# Sets up a basic tmux session taking in an argument as the session name
+# Creates a basic tmux session taking in an argument as the session name
 # Sets up a neovim window and a lazygit window
 # Must be called from within project directory
+# Onus is on the caller to ensure a session does not already exist
 SESH=$1
-tmux has-session -t $SESH 2>/dev/null
+# No existing session
+tmux new-session -d -s $SESH -n "nvim"
 
-if [ $? != 0 ]; then
-    # No existing session
-    tmux new-session -d -s $SESH -n "nvim"
+tmux send-keys -t $SESH:nvim "nvim" C-m
 
-    tmux send-keys -t $SESH:nvim "nvim" C-m
-    
-    tmux new-window -t $SESH -n "lazygit"
-    tmux send-keys -t $SESH:lazygit "lazygit" C-m
+tmux new-window -t $SESH -n "lazygit"
+tmux send-keys -t $SESH:lazygit "lazygit" C-m
 
-    tmux select-window -t "nvim"
-fi
-
-tmux attach-session -t $SESH
+tmux select-window -t "nvim"
--- project_navigator_setup.sh
+++ project_navigator_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 SESH="project_navigator"
-source ~/.config/dotfiles/basic_session_setup.sh $SESH
+tmux has-session -t $SESH 2>/dev/null
+
+if [ $? != 0 ]; then
+    source ~/.config/dotfiles/basic_session_setup.sh $SESH
+fi
+
+tmux attach-session -t $SESH
